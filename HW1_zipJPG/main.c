43cff169266e3b6ee37a02428121e5e336e8c342
Kerosin3 <keromvp@gmail.com>
2022-06-17 17:55:34 +0300

    fread version

--- HW1_zipJPG/main.c
+++ HW1_zipJPG/main.c
@@ -38,12 +38,12 @@ int main(int argc, char *argv[])
 
 int find_valid_jpg(FILE* fp){
 	long unsigned int i = 0;
+	long unsigned int addr = 0;
 	size_t one_byte = 1;
 	size_t flag_st = 0;
 	size_t flag_stop = 0;
-	unsigned char buff[2]; // buf for word
-	unsigned short word = 0xFFFF; //2 bytes
-
+	unsigned char buff[2] = {0}; // buf for word
+	unsigned short word = 0x0000; //2 bytes
 	rewind(fp); //start from the start...
 	if (fseek( fp, 0,SEEK_SET )) { // setting beginning of file
 		printf(" error setting file! \n");
@@ -51,17 +51,16 @@ int find_valid_jpg(FILE* fp){
 		return 0;
 	}
 	do { // opening....
-		fseek(fp,i,SEEK_SET); // shift back one byte
-		if (! fread(buff,one_byte,2,fp)) { // not succesfull read
+		if (! fread(&buff[0],one_byte,1,fp)) { // not succesfull read
 			exit(1);
 		}
 		//printf("0x%X \n",buff[0]);
 		//printf("0x%X \n",buff[1]);
-		word = ((0xFFFF & (buff[0] << 8  )) + buff[1] );
-		printf("0x%X\t\t,i = %d \n",word,i);
+		printf("result = 0x%X\t\t,i = %d \n",buff[0],i);
 		if (word == JPG_START) {
 			printf("found valid beginning\n");
 			flag_st = 1;
+			word = ((0xFFFF & (buff[1] << 8  )) + buff[0] );
 		}
 		if (word == JPG_END) { // found valid EOI section
 			printf("found valid ending\n");
@@ -70,7 +69,12 @@ int find_valid_jpg(FILE* fp){
 		if (feof(fp)) {
 			flag_stop = 1;
 		}
+		if (i % 2) {
+			word = ((0xFFFF & (buff[1] << 8  )) + buff[0] );
+			printf("i= %i, word = 0x%X, addr=0x%x \n",i,word,i*(i%15));
+		}
 		++i;
+		buff[1] = buff[0];
 	} while (!flag_stop); 
 	printf("end of file reached\n");
 	return 0;	// if end of file
