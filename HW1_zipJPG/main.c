7d3c419c808c1f4cdb6eae6ca90680ee586d9989
Kerosin3 <keromvp@gmail.com>
2022-06-19 21:58:56 +0300

    added main2

--- HW1_zipJPG/main.c
+++ HW1_zipJPG/main.c
@@ -4,28 +4,30 @@
 
 #define JPG_START 0xFFD8
 #define JPG_END   0xFFD9
-
+#define DEBUG 	  1
 int find_valid_jpg(FILE* fp);
 
 int main(int argc, char *argv[])
 {
-	signed n_end_jpg = 0;
+	unsigned char end_of_fine = 0;
+	unsigned n_end_jpg = 0;
 	if (argc <= 1) {
 		printf("Please enter filename, aborting..\n");
 		return 0;
 	}
 	FILE* fp; // all is ok
 	//open file
-	if ( (fp = fopen(argv[1],"r")) == "NULL" ) {
+	if ( (fp = fopen(argv[1],"rb")) == "NULL" ) {
 		printf("error while opening the file, aborting..\n");
 		exit(1);
 	}
 	fp = fopen(argv[1],"rb");
 	// end open
 	//
-	n_end_jpg = find_valid_jpg(fp);
-	if (n_end_jpg) {
-		printf("xxxxx %d\n",n_end_jpg );
+	if (n_end_jpg = find_valid_jpg(fp)) {
+		fseek(fp,1,0); // set cursor to end of jpg section
+		fread(&end_of_fine,1,1,fp);
+		printf("--------------------------0x%x\n",end_of_fine);
 	} else {
 		printf("not valid jpg has been found, aborting..\n");
 		exit(1);
@@ -38,12 +40,13 @@ int main(int argc, char *argv[])
 
 int find_valid_jpg(FILE* fp){
 	long unsigned int i = 0;
-	long unsigned int addr = 0;
+	long unsigned int addr = 0x0;
 	size_t one_byte = 1;
 	size_t flag_st = 0;
 	size_t flag_stop = 0;
 	unsigned char buff[2] = {0}; // buf for word
 	unsigned short word = 0x0000; //2 bytes
+	unsigned short start = 0x0000; //2 bytes
 	rewind(fp); //start from the start...
 	if (fseek( fp, 0,SEEK_SET )) { // setting beginning of file
 		printf(" error setting file! \n");
@@ -52,31 +55,40 @@ int find_valid_jpg(FILE* fp){
 	}
 	do { // opening....
 		if (! fread(&buff[0],one_byte,1,fp)) { // not succesfull read
-			exit(1);
+			if (i % 2)  { // if end of file is not even
+				fseek(fp,-2,SEEK_CUR); // set cursor to end of file pre
+				fread(&buff[0],one_byte,1,fp); // real pre last
+				word = ((0xFFFF & (buff[0] << 8  )) + buff[1] );
+				i--;
+				if (DEBUG) printf("i=% 6i, word = 0x%04X,\t page=0x%x \t offset = 0x%x \n",i,word,addr,(i) % 16);
+			} else  {
+				;
+			}// if not full word
 		}
-		//printf("0x%X \n",buff[0]);
-		//printf("0x%X \n",buff[1]);
-		printf("result = 0x%X\t\t,i = %d \n",buff[0],i);
+		//printf("cur tick = 0x%X\t\t,i = %X \n",buff[0],i % 16);
 		if (word == JPG_START) {
-			printf("found valid beginning\n");
 			flag_st = 1;
+			start = i - 1; //previous i
+			printf("found valid jpg beginning %u \n",start);
 			word = ((0xFFFF & (buff[1] << 8  )) + buff[0] );
 		}
 		if (word == JPG_END) { // found valid EOI section
-			printf("found valid ending\n");
+			printf("found valid jpg ending\n");
 			if (flag_st) return i; // start is also found
 		}
 		if (feof(fp)) {
+			printf("end of file found \n");
 			flag_stop = 1;
 		}
+		if  ((! ( (i) % 16)) & (i != 0)) addr+=0x10; // current mem page
 		if (i % 2) {
 			word = ((0xFFFF & (buff[1] << 8  )) + buff[0] );
-			printf("i= %i, word = 0x%X, addr=0x%x \n",i,word,i*(i%15));
+			if (DEBUG) printf("i=% 6i, word = 0x%04X,\t page=0x%x \t offset = 0x%x \n",i,word,addr,i % 16);
 		}
 		++i;
 		buff[1] = buff[0];
 	} while (!flag_stop); 
-	printf("end of file reached\n");
+	printf("end of file reached, something gone wrong\n");
 	return 0;	// if end of file
 		    //
 }
---
+++ HW1_zipJPG/main2.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <stdint.h>
+
+
+
+#define DEBUG 	  0 
+#define MAX_WORD  8
+signed long long read_bytes(FILE*,unsigned,unsigned, ...); //retuns last byte number
+unsigned long find_valid_jpg(FILE*);
+unsigned long find_zip_filename(FILE*);
+unsigned long extract_filename(FILE*, unsigned long offset);
+
+unsigned global_offset = 0x0;
+
+int main(int argc, char *argv[])
+{
+	unsigned long long jpg_end = 0x0;
+	unsigned long first_signature_zip = 0x0;
+	if (argc <= 1) {
+		printf("Please enter filename, aborting..\n");
+		return 0;
+	}
+	FILE* fp; // all is ok
+	//open file
+	if ( (fp = fopen(argv[1],"rb")) == "NULL" ) {
+		printf("error while opening the file, aborting..\n");
+		exit(1);
+	}
+	fp = fopen(argv[1],"rb");
+	if (!(jpg_end = find_valid_jpg(fp))) exit(1); // find whether this is a valid jpg file
+	first_signature_zip = find_zip_filename(fp);
+	if (first_signature_zip) printf("first zip signature offset is: 0x%X\n",first_signature_zip);
+	extract_filename(fp,first_signature_zip);
+	printf("-----%x\n" ,read_bytes(fp,global_offset,2,0x48,0x2C)); 
+	// end open
+	//
+	//printf("the offset 0x%x \n", read_bytes(fp,2,0x32,0x29));
+	//readed_bytes = read_bytes(fp,4,0x50,0x4b,0x03,0x04);
+
+	fclose(fp);
+	return 0;
+}
+
+unsigned long extract_filename(FILE* fp,unsigned long offset) {
+		offset+=1; // read from the next position
+		printf("current offset is %X \n",offset);
+		const unsigned filename_length = 2; // for length, not for filename itself
+		printf("searching first filename\n");
+		const unsigned long len_offset =  0x16;  // 0x10 + 0xA - 4
+		//const unsigned long fiename_offset =  0x20 - 0x4;  // 0x10 + 0xA - 4
+		unsigned length_of_filename = 0x0 ;
+		offset += len_offset; // add offseti to filename len field
+		unsigned char buf[filename_length];
+		fseek(fp,offset,SEEK_SET); //set position to len filename field
+		fread(buf,1,filename_length,fp); // read 2 bytes for filename
+		for (size_t i = 0; i < filename_length; ++i) {
+			if (DEBUG) printf("filename length 0x%X\n",buf[i]);		
+		}
+		length_of_filename  = buf[0] + (buf[1] << 8); //add to total filename len
+		unsigned char filename_chars[17]; // filename 17 byles max
+		if (DEBUG) printf("length of filename is %d \n",length_of_filename);
+		fseek(fp,offset+4,SEEK_SET);  // filename fiels is just 2 bytes next
+		fread(filename_chars,1,length_of_filename,fp); 
+		if (DEBUG)for (size_t i = 0;  i < length_of_filename; ++ i) {
+			printf("char: 0x%X %c \n",filename_chars[i],filename_chars[i]);
+		}
+		printf("name of a compressed file: ");
+		for (size_t i = 0; i < length_of_filename; ++i) {
+			printf("%c",filename_chars[i]);
+		} printf("\n");
+		global_offset = offset + 0xF + 0x7; // set global adress to extra fiel offse
+		printf("nex offset is %x \n",global_offset);
+		//unsigned char filename[18]; // max filename
+		//fseek(fp,offset,SEEK_SET); //set position to ..
+			
+}
+unsigned long find_zip_filename(FILE* fp){
+	printf("searching first local file zip signature\n");
+	unsigned const*const header_start[4] = {
+		0x50,
+		0x4B,
+		0x03,
+		0x04,
+	};
+	signed long long signarute_offset = 0x0;
+	signarute_offset = read_bytes(fp,0,4,header_start[0],header_start[1],header_start[2],header_start[3]);
+	if (signarute_offset >= 0){
+		printf("found valid zip signature at offset: 0x%X\n",signarute_offset);
+		return (unsigned long) signarute_offset; // return first signature offset
+	} else {
+		printf("not valid zip signature has been found\n");
+		return 0;
+	}
+	return 0;
+
+}
+
+unsigned long find_valid_jpg(FILE* fp) {
+	printf("searching valid jpg image\n");
+	unsigned const*const jpg_start[2] = {
+		0xFF,
+		0xD8,
+	};
+	unsigned short const*const jpg_end[2] = {
+		0xFF,
+		0xD9,
+	};
+	unsigned long out_func = 0;
+	signed long long  out = 0;
+	if ( (out = read_bytes(fp,0,2,jpg_start[0],jpg_start[1]))  > 0 ) {
+		printf("found valid jpg signature at: 0x%X\n",out);
+	} else {
+		printf("valid jpg signature has not been found, aborting..\n");
+		return 0; // beginning not found
+	}
+	if ( (out = read_bytes(fp,0,2,jpg_end[0],jpg_end[1]))  > 0 ) {
+		printf("found valid end jpg signature at: 0x%X\n",out);
+		printf("--------------------------------------------\n");
+		out_func = (unsigned) (out);
+		return out_func; // rerutn end position  
+		}
+	else {
+		printf("not valid jpg image has been found\n");
+		return 0;// end not found
+		}
+}
+signed long long read_bytes(FILE* fp,unsigned skip, unsigned n_width, ...){
+	va_list ap;
+	va_start(ap,n_width);
+	if (n_width <= 1 ) return -1; // not valid
+	if (n_width > MAX_WORD) return -1; // more than max
+	unsigned char searched_comp[MAX_WORD] = {0}; //searched bytes
+	uint64_t searched_pattern = 0x0000000000000000; // all zeros
+	for (size_t i = 0; i < n_width ; ++i) { // assign bytes to array
+		searched_comp[i] = (unsigned char) va_arg(ap, int); // INT????
+		if (DEBUG) printf("you paseed 0x%X\n",searched_comp[i]);
+	}
+	uint64_t whole_word = 0x0000000000000000; // all zeros
+	unsigned long stop_byte= 0;
+	size_t t = 1;
+	searched_pattern = searched_comp[0]; // combine consequitive bytes
+		do {
+			searched_pattern = searched_pattern << 8;
+			searched_pattern = searched_pattern | searched_comp[t] ;
+			t++;
+		} while (t != n_width);
+	printf("searched pattern is 0x%X\n",searched_pattern);
+
+
+	unsigned char* buffer = (unsigned char*) malloc(sizeof(unsigned char)*n_width); //allocate for buf
+	unsigned long offset_byte = 0;
+	unsigned char byte_readed = 0x00;
+	rewind(fp); //start from the start...
+	while (!(feof(fp))) { //read from start
+		if (DEBUG) printf("i= %d\n",offset_byte);
+		fseek(fp, offset_byte + skip ,SEEK_SET); // set 0,1,2,3 byte
+		fread(buffer, 1, n_width ,fp);  // read byte * n width
+		for (size_t i = 0; i < n_width; ++i) {
+			//printf("readed: 0x%X\n",buffer[i]);
+		}
+		size_t c = 1;
+		whole_word = buffer[0]; // combine consequitive bytes
+		do {
+			whole_word = whole_word << 8;
+			whole_word = whole_word | buffer[c] ;
+			c++;
+		} while (c != n_width);
+		if (DEBUG) printf("current pattern is: 0x%X\n",whole_word);
+		if (whole_word == searched_pattern) {
+			stop_byte = (offset_byte + n_width + skip) -1;
+			if (DEBUG) printf("found sequence, last byte offset: %d \n",stop_byte );
+			free(buffer);
+			return (signed long long) stop_byte;
+		}
+		++offset_byte;		      //
+	
+	}
+	printf("EOF reached, section has not been found \n");
+	free(buffer);
+	va_end(ap);
+	return -2;
+}
+
