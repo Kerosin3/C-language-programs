1b30adf0b9ab814775ee68ea738107c2cc8fe4eb
Kerosin3 <keromvp@gmail.com>
2022-07-06 22:51:06 +0300

    added test file

--- hash_table/main.c
+++ hash_table/main.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "spookyhash/src/spookyhash_api.h"
+#include <stdlib.h>
+//#include "spookyhash/src/spookyhash_api.h"
 #include "BSD_strings.c"
+
+typedef union {
+	uint8_t byte0;
+	uint8_t byte1;
+	uint8_t byte2;
+	uint8_t byte3;
+} utf8_generic;
+
+typedef struct {
+	utf8_generic symbl;
+	unsigned long s_count;
+	unsigned long hash;
+
+} utf8_char;
+
 
 int main(int argc, char *argv[])
 {
-	uint32_t seedx = 0x12ab;
+	unsigned a = 0b1000;
+	printf("youvalue is %u\n",a);
 
-        char u_input[21];
-	fgets(u_input,21,stdin);
-	unsigned len = strnlen(u_input,20);
-	printf("you entered:%s, length is %d\n",u_input,len);
-	uint32_t hash32 = spookyhash_32(u_input, len-1, seedx);
-	printf("hash value is %lu \n",hash32);
+        //char u_input[21];
+	//fgets(u_input,21,stdin);
+	//unsigned len = strnlen(u_input,20);
+	//printf("you entered:%s, length is %d\n",u_input,len);
+	FILE* fp_r;
+	if ((fp_r = fopen(argv[1],"rb")) == NULL) {
+		printf("error opening file, aborting...\n");
+		exit(1);
+	}
+
+	fclose(fp_r);
 	return 0;
 }
