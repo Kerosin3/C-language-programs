70844eae70594d881966dd17cfac5140f5064444
Kerosin3 <keromvp@gmail.com>
2022-07-09 19:36:25 +0300

    calloc version word assignment

--- hash_table/hash_f.c
+++ hash_table/hash_f.c
@@ -2,22 +2,21 @@
 #define IS_UNSIGNED(t) ((t)~1 > 0)
 unsigned long long calc_hash(uint8_t* input_string){
 	//printf("hash - %s \n",(char*) input_string);
-	printf(" is unsigned??? %d\n",IS_UNSIGNED(char));
 	if (sizeof(char) != sizeof(uint8_t)) {
 		printf("platform error!,aborting...\n");
 		return 0;
 	}
-	char* str_ptr =(char*) input_string;
-	//printf("len of string %d\n",strnlen(str_ptr,25)); // 25 - max word length 
-	size_t str_size= strnlen(str_ptr,25); // size of analyzed string
-	char* to_analyze[str_size+1];// plus null terminator
-	strlcpy(to_analyze,str_ptr,26);
-	size_t l_to_analyze = strnlen(str_ptr,25);
-	printf("copied string is %s, its length is %d \n",to_analyze,strnlen(str_ptr,25));
+	char* str_ptr =(char*) input_string; // its ok
+	//size_t str_size= strnlen(str_ptr,25); // size of analyzed string dont know max size...
+	size_t str_size= strlen(str_ptr); // size of analyzed string
+	//char* to_analyze[str_size+1];// assign now string plus null terminator
+	//strcpy(to_analyze,str_ptr); // no strncpy...copy string...
+	//size_t l_to_analyze = strnlen(str_ptr,25);
+	printf("copied string is %s, its length is %d \n",str_ptr,str_size);
 	unsigned long long temp_c= 0;
-	for (size_t i = 0; i < l_to_analyze; ++i) {// not includes null terminator
-		temp_c+=( ((uint8_t) to_analyze[i] ) * pow(((unsigned) SALT),i)  ); // unicode is always 7 byte with leading zero
-		//printf("value is 0x%X, your hash is %llu\n",(uint8_t) to_analyze[i],temp_c);
+	for (size_t i = 0; i < str_size; ++i) {// not includes null terminator
+		temp_c+=( ((uint8_t) str_ptr[i] ) * pow(((unsigned) SALT),i)  ); // unicode is always 7 byte with leading zero
+		//printf("value is 0x%X, your hash is %llu\n",(uint8_t) str_ptr[i],temp_c);
 	}
 	return temp_c;
 	
--- hash_table/main.c
+++ hash_table/main.c
@@ -39,12 +39,13 @@ int main(int argc, char *argv[])
 	while ( (( some_word = find_a_word(fp_r) ).word)  ){ // WHILE NOT NULL
 		//some_word = find_a_word(fp_r);
 		printf("%d,your word is \"%s\"\n",END_REACHED,(char*) (some_word.word));
-		hash_val=calc_hash((char*) (some_word.word));
+		hash_val=calc_hash((char*) (some_word.word)); // memory is assigned here
+		free(some_word.word);
 		printf("value of has is %llu\n",hash_val);
 	}
 
 	fclose(fp_r);
-	free(some_word.word);
+	//free(some_word.word);
 	return 0;
 }
 
@@ -53,8 +54,7 @@ word_pointers find_a_word(FILE* fp){
 	long int beg = 0;
 	long int end = 0;
 	int width = 0;
-	uint8_t* a_word = (uint8_t*) calloc(MAX_WORD_LENGTH+1,sizeof(uint8_t) );
-	beg = ftell(fp);// set beginning of the word
+	beg = ftell(fp);//set beginning to current position
 	word_pointers out;
 	out.begin = beg; 
 	unsigned i = 0;
@@ -63,23 +63,38 @@ word_pointers find_a_word(FILE* fp){
 		#ifdef DEBUG
 			printf("EOF REACHED!!\n");
 		#endif
+			end = ftell(fp);
+			out.end = end; 
 			END_REACHED = 1;
-			//out.word = a_word; 
-			break;
+			out.word = NULL; 
+			return out;
 		}
 		fread(&read_char,1,1,fp); // read one byte and move cursor
-		a_word[i]=read_char;	
 		i++;
 		#ifdef DEBUG
 			//printf("readed 0x%X, is space =%X \n",read_char,isspace(read_char));
 		#endif
 	} while ( !(isspace(read_char))  ); //not space or EOF
-	a_word[i-1]='\0'; // set null symbol
+	i--; //decrement last i, i+1 == total elements need
+	uint8_t* a_word = (uint8_t*) calloc(i+1,sizeof(uint8_t) );// one for NULL TERMINATOR
+	if (!(a_word)) printf("ERROR WHILE ASSIGNING WORD, WHAT IS ITS LENGTH???\ni");
+	uint8_t* ptr_a_word = a_word;
+	signed convert = ~i ; // convert = (-i + 1) ONE CHAR MORE
+	//printf("value of if is %u, negative is %d \n",i,convert);
+	fseek(fp,convert,SEEK_CUR); // set cursor back
+	for (size_t j = 0; j < i; ++j) { //read content 0.1.2..6
+		fread(&read_char,1,1,fp); // read one byte 
+		ptr_a_word[j] = read_char;
+	}
+	ptr_a_word[i]='\0'; // set null symbol
 	end = ftell(fp);
 	out.end = end; 
 	width = end - beg; // no need to add extra for null
-	a_word = (END_REACHED) ? NULL :  realloc(a_word,i*sizeof(uint8_t)); // i size is ok
+	a_word = (END_REACHED) ? NULL :  a_word; // i size is ok
 	out.word = a_word;
+	do {//skip whitepaces
+		fread(&read_char,1,1,fp);
+	} while (!(isspace(read_char)));
 	#ifdef DEBUG
 		printf("space chr  has been found!\n");
 		printf("beg=%lu,end=%lu \n",out.begin,out.end);
