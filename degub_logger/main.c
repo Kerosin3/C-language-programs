89fdf842e7f4bca9e0bc8b1158309d683d81c22b
Kerosin3 <keromvp@gmail.com>
2022-09-16 17:23:20 +0300

    logger debug v2

--- degub_logger/main.c
+++ degub_logger/main.c
@@ -2,21 +2,41 @@
 #include <stdlib.h>
 #include <execinfo.h>
 
-void func1(){
-	volatile x = 5;
-	volatile y = 666;
-//	printf("current func is %s\n",__func__);
-//	printf("current line is %d\n",__LINE__);
+#define SOME() func1(__LINE__);
+
+typedef enum log_level{
+	debug,info,warning,error
+} log_level;
+
+void func1(int linen){
 	void* array[4096];
+	printf("line n is %d\n",linen);
 	size_t size = backtrace(array,4096);
 	backtrace_symbols_fd(array,size,1);
 	puts("");
 }
- 
+
+void test_fun(){
+	printf("we are here\n");
+	char* kaka = malloc(10);
+	SOME();
+}
+
+FILE* write_to_log(char filename[static 1]){
+	FILE* log_file;
+	log_file = fopen(filename,"a+" );
+	if (!log_file) {
+		printf("error while opening the log file\n");
+		exit(1);
+	}
+	return log_file;
+}
+
 int main(int argc, char *argv[]){
 
-
-	func1();
+	SOME();
+	test_fun();
+	
 	return 0;
 }
 
