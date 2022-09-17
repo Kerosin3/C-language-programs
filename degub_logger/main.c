a4a3b6b6d14050569cdd98398426ee981dccf96f
Kerosin3 <keromvp@gmail.com>
2022-09-17 10:30:40 +0300

    debug v3

--- degub_logger/main.c
+++ degub_logger/main.c
@@ -20,14 +20,7 @@
 #define WARNING 2
 #define ERROR 3
 
-//#define WRAP_ERRORS(...) write_backlog(__LINE__,__VA_ARGS__);
-//#define WRAP_LOGGING(...) write_log(__LINE__, __VA_ARGS__);
 #define write_to_log(...) wrap_gglog(__LINE__, __VA_ARGS__);
-
-FILE *get_file(char filename[static 1]);
-static void write_to_file(FILE *log_file, int n, ...);
-
-void write_gglog(int, char *filename, unsigned LOG_LEVEL, char *message);
 
 typedef enum log_level
 {
@@ -37,16 +30,30 @@ typedef enum log_level
     error
 } log_level;
 
-char *get_level(log_level);
+typedef struct internal_init
+{
+    char *filename;
+} internal_init;
 
-char *get_level(log_level level)
+static internal_init init_t;
+
+static char *get_level(log_level);
+static void write_backlog(int linen, char filename[static 1], char *message);
+static FILE *get_file(char filename[static 1]);
+static void write_to_file(FILE *log_file, int n, ...);
+static void write_gglog(int, unsigned LOG_LEVEL, char *message);
+static void write_log(int lineno, char filename[static 1], log_level level, char *message);
+void wrap_gglog(int lineno, unsigned LOG_LEVEL, char *message);
+int init_recording(char *filename);
+
+static char *get_level(log_level level)
 {
 
     char *levels[4] = {"debug", "info", "warning", "error"};
     return levels[level];
 }
 
-void write_backlog(int linen, char filename[static 1], char *message)
+static void write_backlog(int linen, char filename[static 1], char *message)
 {
     static const char *start_bt = "-----------------LOG:ERRORS,BACKTRACE-------------------\n";
     static const char *end_bt = "--------------------------------------------------------\n";
@@ -115,7 +122,7 @@ static void write_to_file(FILE *log_file, int n, ...)
     fclose(log_file);
 }
 
-void write_log(int lineno, char filename[static 1], log_level level, char *message)
+static void write_log(int lineno, char filename[static 1], log_level level, char *message)
 {
     char buf[MAX_MSG] = {0};
     if (strnlen(message, MAX_MSG) == MAX_MSG)
@@ -136,24 +143,15 @@ void write_log(int lineno, char filename[static 1], log_level level, char *messa
 
     write_to_file(file_log, 4, sn, ln, buf, end_bt);
 }
-/*
-void wrap_errors(char* filename,char* message)
-{
-    WRAP_ERRORS(filename,message);
-}
-void wrap_logging(char* filename,unsigned log_level,char* message)
-{
-    WRAP_LOGGING(filename, log_level, message);
-}
-*/
 
-void wrap_gglog(int lineno, char *filename, unsigned LOG_LEVEL, char *message)
+void wrap_gglog(int lineno, unsigned LOG_LEVEL, char *message)
 {
-    write_gglog(lineno, filename, LOG_LEVEL, message);
+    write_gglog(lineno, LOG_LEVEL, message);
 }
 
-void write_gglog(int lineno, char *filename, unsigned LOG_LEVEL, char *message)
+static void write_gglog(int lineno, unsigned LOG_LEVEL, char *message)
 {
+    char *filename = init_t.filename;
     if ((LOG_LEVEL) < 0 || (LOG_LEVEL) > 3)
     {
         printf("error setting log level,setting to default");
@@ -175,8 +173,23 @@ void write_gglog(int lineno, char *filename, unsigned LOG_LEVEL, char *message)
     }
     write_log(lineno, filename, LOG_LEVEL, msg);
 }
+/*
+ * when open is ok return 1, else zero
+ */
+int init_recording(char *filename)
+{
+    init_t.filename = filename;
+    FILE *ret;
+    if (!(ret = get_file(filename)))
+    {
+        printf("error open file to logging\n");
+        return 0;
+    }
+    fclose(ret);
+    return 1;
+}
 
-FILE *get_file(char filename[static 1])
+static FILE *get_file(char filename[static 1])
 {
     FILE *log_file;
     log_file = fopen(filename, "a+");
@@ -191,9 +204,10 @@ FILE *get_file(char filename[static 1])
 int main(int argc, char *argv[])
 {
 
-    write_to_log("dede", WARNING, "hohohoohoho");
-    write_to_log("dede", ERROR, "hohohodasdadasdaohoho");
-    write_to_log("dede", INFO, "hohohodas1111111111dadasdaohoho");
+    init_recording("ggagaga");
+    write_to_log(WARNING, "hohohoohoho");
+    write_to_log(ERROR, "hohohodasdadasdaohoho");
+    write_to_log(INFO, "hohohodas1111111111dadasdaohoho");
 
     return 0;
 }
